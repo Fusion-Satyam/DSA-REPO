f9098843bba03ef0f24056b3e4028058d8a86cc7
Satyam raj <satyamraj2779@gmail.com>
2025-09-12 23:25:10 +0530

    Linked list in the main class created

--- DSA/dsa_Linkedlist/Step1.java
+++ DSA/dsa_Linkedlist/Step1.java
@@ -1,6 +1,8 @@
 package dsa_Linkedlist;
 
-class Node{
+
+public class Step1 {
+    public static class Node{
     int data ;
     Node next;
     Node(int data ){
@@ -9,9 +11,15 @@ class Node{
     }
 }
 
-public class Step1 {
+public static Node head ;
+public static Node tail ;
+
     public static void main(String[] args) {
-        
+        Step1 ll = new Step1();
+        ll.head = new Node(1);
+        ll.head.next = new Node(2);
+        ll.head.next.next = new Node(3);
+        ll.tail = ll.head;
     }
     
 }
