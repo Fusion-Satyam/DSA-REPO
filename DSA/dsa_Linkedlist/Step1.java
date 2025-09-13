b647a1bce048c262bce0043887d1af1059b68208
Satyam raj <satyamraj2779@gmail.com>
2025-09-13 21:47:44 +0530

    Created addlast and printlist Method

--- DSA/dsa_Linkedlist/Step1.java
+++ DSA/dsa_Linkedlist/Step1.java
@@ -31,10 +31,38 @@ public void addFirst(int data){
     head = newNode;
 }
 
+
+public void addLast(int data){
+    if(head == null){
+        Node newNode = new Node(data);
+        head = tail= newNode;
+        return;
+    }
+
+    // step 1 : create new node
+    Node newNode = new Node(data);
+
+    // step 2 : tail next = newNode
+    tail.next = newNode;
+
+    // step 3 : tail = newNode
+    tail = newNode;
+}
+
+public void printList(){
+    Node  temp = head;
+    while(temp!=null){
+        System.out.print(temp.data+"->" );
+        temp = temp.next;
+    }
+    System.out.println("null");
+}
+
     public static void main(String[] args) {
         Step1 ll = new Step1();
         ll.addFirst(0);
         ll.addFirst(1);
+        ll.printList();
 
     }
 }
