ebcf2d5761f8ef56cd8d3b51bff23a8d0ebd08a6
Satyam raj <satyamraj2779@gmail.com>
2025-09-13 21:17:26 +0530

    Created a addfirst function

--- DSA/dsa_Linkedlist/Step1.java
+++ DSA/dsa_Linkedlist/Step1.java
@@ -14,12 +14,27 @@ public class Step1 {
 public static Node head ;
 public static Node tail ;
 
+
+public void addFirst(int data){
+
+    if(head == null){
+        Node newNode = new Node(data);
+        head = tail = newNode;
+        return;
+    }
+
+    // step 1 : create new node
+    Node newNode = new Node(data);
+    // step 2 : newNode next = head
+    newNode.next = head;
+    // step 3 : head = newNode
+    head = newNode;
+}
+
     public static void main(String[] args) {
         Step1 ll = new Step1();
-        ll.head = new Node(1);
-        ll.head.next = new Node(2);
-        ll.head.next.next = new Node(3);
-        ll.tail = ll.head;
+        ll.addFirst(0);
+        ll.addFirst(1);
+
     }
-    
 }
