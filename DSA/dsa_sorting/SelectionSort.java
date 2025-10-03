a8a4345974fb3444f83fe5aaba45dc7562fc23b9
Satyam raj <satyamraj2779@gmail.com>
2025-10-03 23:20:15 +0530

    Selection Sort

---
+++ DSA/dsa_sorting/SelectionSort.java
@@ -0,0 +1,45 @@
+package dsa_sorting;
+
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap if needed
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {29, 10, 14, 37, 13};
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+    }
+}
